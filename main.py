2e480780f2cb33a63acadf3300a0f8ec37a03b37
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2025-05-29 16:37:15 +0300

    new

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class UserData(BaseModel):
     last_name: str
     avatar: str
 
+class UserDataCreate(BaseModel):
+    name: str
+    job: str
 
 class ResourceData(BaseModel):
     id: int
@@ -48,104 +51,89 @@ class ResponseModelListResource(BaseModel):
     support: SupportData
 
 
-@app.get("/api/users", response_model=ResponseModelList)
-def get_list_users(page: int = 2):
-    users = {
-        2: {
-            "page": 2,
-            "per_page": 6,
-            "total": 12,
-            "total_pages": 2,
-            "data": [
-                {
-                    "id": 7,
-                    "email": "michael.lawson@reqres.in",
-                    "first_name": "Michael",
-                    "last_name": "Lawson",
-                    "avatar": "https://reqres.in/img/faces/7-image.jpg"
-                },
-                {
-                    "id": 8,
-                    "email": "lindsay.ferguson@reqres.in",
-                    "first_name": "Lindsay",
-                    "last_name": "Ferguson",
-                    "avatar": "https://reqres.in/img/faces/8-image.jpg"
-                },
-                {
-                    "id": 9,
-                    "email": "tobias.funke@reqres.in",
-                    "first_name": "Tobias",
-                    "last_name": "Funke",
-                    "avatar": "https://reqres.in/img/faces/9-image.jpg"
-                },
-                {
-                    "id": 10,
-                    "email": "byron.fields@reqres.in",
-                    "first_name": "Byron",
-                    "last_name": "Fields",
-                    "avatar": "https://reqres.in/img/faces/10-image.jpg"
-                },
-                {
-                    "id": 11,
-                    "email": "george.edwards@reqres.in",
-                    "first_name": "George",
-                    "last_name": "Edwards",
-                    "avatar": "https://reqres.in/img/faces/11-image.jpg"
-                },
-                {
-                    "id": 12,
-                    "email": "rachel.howell@reqres.in",
-                    "first_name": "Rachel",
-                    "last_name": "Howell",
-                    "avatar": "https://reqres.in/img/faces/12-image.jpg"
-                }
-            ],
-            "support": {
-                "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-                "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
+users = {
+    2: {
+        "page": 2,
+        "per_page": 7,
+        "total": 13,
+        "total_pages": 2,
+        "data": [
+            {
+                "id": 2,
+                "email": "janet.weaver@reqres.in",
+                "first_name": "Janet",
+                "last_name": "Weaver",
+                "avatar": "https://reqres.in/img/faces/2-image.jpg"
+            },
+            {
+                "id": 7,
+                "email": "michael.lawson@reqres.in",
+                "first_name": "Michael",
+                "last_name": "Lawson",
+                "avatar": "https://reqres.in/img/faces/7-image.jpg"
+            },
+            {
+                "id": 8,
+                "email": "lindsay.ferguson@reqres.in",
+                "first_name": "Lindsay",
+                "last_name": "Ferguson",
+                "avatar": "https://reqres.in/img/faces/8-image.jpg"
+            },
+            {
+                "id": 9,
+                "email": "tobias.funke@reqres.in",
+                "first_name": "Tobias",
+                "last_name": "Funke",
+                "avatar": "https://reqres.in/img/faces/9-image.jpg"
+            },
+            {
+                "id": 10,
+                "email": "byron.fields@reqres.in",
+                "first_name": "Byron",
+                "last_name": "Fields",
+                "avatar": "https://reqres.in/img/faces/10-image.jpg"
+            },
+            {
+                "id": 11,
+                "email": "george.edwards@reqres.in",
+                "first_name": "George",
+                "last_name": "Edwards",
+                "avatar": "https://reqres.in/img/faces/11-image.jpg"
+            },
+            {
+                "id": 12,
+                "email": "rachel.howell@reqres.in",
+                "first_name": "Rachel",
+                "last_name": "Howell",
+                "avatar": "https://reqres.in/img/faces/12-image.jpg"
             }
+        ],
+        "support": {
+            "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+            "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
         }
     }
+}
 
+
+@app.get("/api/users", response_model=ResponseModelList)
+def get_list_users(page: int = 2):
     user_data = users.get(page)
     if not user_data:
         raise HTTPException(status_code=404, detail="Page not found")
 
-    return {
-        "page": user_data["page"],
-        "per_page": user_data["per_page"],
-        "total": user_data["total"],
-        "total_pages": user_data["total_pages"],
-        "data": user_data["data"],
-        "support": user_data["support"],
-    }
+    return user_data
 
 
 @app.get("/api/users/{user_id}", response_model=ResponseModel)
 def get_single_user(user_id: int):
-    user = {
-        2: {
-            "id": 2,
-            "email": "janet.weaver@reqres.in",
-            "first_name": "Janet",
-            "last_name": "Weaver",
-            "avatar": "https://reqres.in/img/faces/2-image.jpg",
-        }
-    }
+    for page_data in users.values():
+        for user in page_data["data"]:
+            if user["id"] == user_id:
+                return {"data": user,
+                        "support": page_data["support"]}
 
-    support_info = {
-        "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-        "text": "Tired of writing endless social media content? Let Content Caddy generate it for you.",
-    }
-
-    user = user.get(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return {
-        "data": user,
-        "support": support_info,
-    }
+    raise HTTPException(status_code=404, detail="User not found")
 
 
 @app.get("/api/unknown", response_model=ResponseModelListResource)
@@ -220,6 +208,9 @@ def get_list_resource():
         "support": resource_data["support"],
     }
 
+@app.post("/api/users")
+def create_user(user: UserDataCreate):
+    return user
 
 if __name__ == "__main__":
     import uvicorn
--- test_reqres.py
+++ test_reqres.py
@@ -4,13 +4,15 @@ from jsonschema import validate
 
 from schemas import (get_list_user_schema, get_single_user_schema, get_list_resource_schema)
 
+BASE_URL = "http://127.0.0.1:8000"
+
 
 @pytest.mark.parametrize("page_number", [
     (2)
 ])
 def test_api_list_users_status_code_200(page_number):
     #url = f"https://reqres.in/api/users?page={page_number}"
-    url = f"http://127.0.0.1:8000/api/users?page={page_number}"
+    url = f"{BASE_URL}/api/users?page={page_number}"
     response = requests.get(url)
     assert response.status_code == 200
 
@@ -20,7 +22,7 @@ def test_api_list_users_status_code_200(page_number):
 ])
 def test_api_list_users_response_not_empty(page_number):
     #url = f"https://reqres.in/api/users?page={page_number}"
-    url = f"http://127.0.0.1:8000/api/users?page={page_number}"
+    url = f"{BASE_URL}/api/users?page={page_number}"
     response = requests.get(url)
     data = response.json()
     assert len(data['data']) > 0
@@ -29,9 +31,9 @@ def test_api_list_users_response_not_empty(page_number):
 @pytest.mark.parametrize("page_number", [
     (2)
 ])
-def test_api_list_user_validate_response_schema(page_number):
+def test_api_list_users_validate_response_schema(page_number):
     #url = f"https://reqres.in/api/users?page={page_number}"
-    url = f"http://127.0.0.1:8000/api/users?page={page_number}"
+    url = f"{BASE_URL}/api/users?page={page_number}"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     body = response.json()
@@ -43,7 +45,7 @@ def test_api_list_user_validate_response_schema(page_number):
 ])
 def test_api_single_user_status_code_200(user_id):
     #url = f"https://reqres.in/api/users/{user_id}"
-    url = f"http://127.0.0.1:8000/api/users/{user_id}"
+    url = f"{BASE_URL}/api/users/{user_id}"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
@@ -54,7 +56,7 @@ def test_api_single_user_status_code_200(user_id):
 ])
 def test_api_single_user_validate_response_schema(user_id):
     #url = f"https://reqres.in/api/users/{user_id}"
-    url = f"http://127.0.0.1:8000/api/users/{user_id}"
+    url = f"{BASE_URL}/api/users/{user_id}"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     body = response.json()
@@ -63,7 +65,7 @@ def test_api_single_user_validate_response_schema(user_id):
 
 def test_api_list_resource_status_code_200():
     #url = "https://reqres.in/api/unknown"
-    url = f"http://127.0.0.1:8000/api/unknown"
+    url = f"{BASE_URL}/api/unknown"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
@@ -71,7 +73,7 @@ def test_api_list_resource_status_code_200():
 
 def test_api_list_resource_response_not_empty():
     #url = "https://reqres.in/api/unknown"
-    url = f"http://127.0.0.1:8000/api/unknown"
+    url = f"{BASE_URL}/api/unknown"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     data = response.json()
@@ -80,11 +82,17 @@ def test_api_list_resource_response_not_empty():
 
 def test_api_list_resource_validate_response_schema():
     #url = "https://reqres.in/api/unknown"
-    url = f"http://127.0.0.1:8000/api/unknown"
+    url = f"{BASE_URL}/api/unknown"
     headers = {'x-api-key': 'reqres-free-v1'}
     response = requests.get(url, headers=headers)
     body = response.json()
     validate(body, get_list_resource_schema)
+
+def test_api_create_user_status_code_200():
+    response = requests.post(#"https://reqres.in/api/users",
+                             f"{BASE_URL}/api/users",
+                             data={"name": "morpheus", "job": "leader"})
+    assert response.status_code == 201
 
 
 
